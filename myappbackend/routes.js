d9f6604d947de277cfb0d6f60fb4670a347ee981
RamilAllahverdiyev <ramil.asasin.01@gmail.com>
2021-11-29 00:43:44 +0300

    Database

---
+++ myappbackend/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const signUpTemplateCopy = require('../myappbackend/models/SignUpModels')
+
+router.post('/signup', (request, response) => {
+    const signedUpUser = new signUpTemplateCopy({
+        firstName: request.body.firstName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password,
+    })
+
+
+    signedUpUser.save()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+
+        })
+
+})
+
+
+module.exports = router;
--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -23,20 +23,21 @@ class SignUp extends React.Component {
                 type="email"
                 placeholder="Enter email"
               />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                className="PasswordForm"
+                type="password"
+                placeholder="Password"
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
           </Form>
           <div className="Submit">
-            <Button variant="primary" type="submit">
+            <Button className="SubmitButton" variant="primary" type="submit">
               Submit
             </Button>
           </div>
